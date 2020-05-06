a0072efb5f3c815877ee2955591b4a1dc91e0b42
fatihmgenc <47807713+fatihmgenc@users.noreply.github.com>
2020-05-06 11:11:15 +0200

    Iteration fix

--- Game_Of_Life_Task2/Game_Of_Life_Task2.cpp
+++ Game_Of_Life_Task2/Game_Of_Life_Task2.cpp
@@ -186,27 +186,20 @@ int main(int argc, char* argv[])
     int TableSizeX = vm["sizeX"].as<int>();
     int TableSizeY = vm["sizeY"].as<int>();
     int speed = vm["speed"].as<int>();
+    int iterations  = vm["iterations"].as<int>();
     // creating 2d boardgame 
     vector<vector<bool>> GameTable(TableSizeX, vector<bool>(TableSizeY, 0));
 
     //game flow
     PrintTable(GameTable);
     SetCoordinates(GameTable);
-    while (true) {
+    while (iterations>0) {
         system("cls");
         CheckConditions(GameTable);
         PrintTable(GameTable);
+        cout << to_string(iterations) + " iterations left";
         Sleep(2500/speed);
+        iterations--;
     }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
